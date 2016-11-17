74c484c69dd79bfdcb40cc2d39227932b5c4740d
lquishpi <lquishpi@senscyt.gob.ec>
2016-11-17 08:51:41 -0500

    Update: Agregado atributos

--- src/ec/webmarket/entity/Student.java
+++ src/ec/webmarket/entity/Student.java
@@ -2,4 +2,23 @@ package ec.webmarket.entity;
 
 public class Student {
 
+	private Integer id;
+	private String nombre;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 }
