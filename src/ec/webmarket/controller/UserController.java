010375175bb2a9dd0dd44d4d47bb82a2ba40df5f
lquishpi <lquishpi@senscyt.gob.ec>
2016-11-17 08:45:54 -0500

    Update: Agregado atributos a usercontroller

--- src/ec/webmarket/controller/UserController.java
+++ src/ec/webmarket/controller/UserController.java
@@ -1,8 +1,27 @@
 package ec.webmarket.controller;
 
 public class UserController {
+	
+	private String user;
+	private String password;
 
 	public UserController(){
 		
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
