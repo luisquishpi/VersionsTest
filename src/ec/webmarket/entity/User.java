dd171e91b65e3a895850facf758ae6ed7b3d57f7
lquishpi <lquishpi@senscyt.gob.ec>
2016-11-22 09:26:08 -0500

    Agregado anotaciones de entidad

--- src/ec/webmarket/entity/User.java
+++ src/ec/webmarket/entity/User.java
@@ -1,7 +1,14 @@
 package ec.webmarket.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+	@Id
 	private Integer id;
+
 	private String username;
 	private String password;
 
